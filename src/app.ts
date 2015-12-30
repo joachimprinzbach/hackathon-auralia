f83099307bf141709a1f6fd889f603076ac4a62c
joachimprinzbach <joachim.prinzbach@gmail.com>
2015-12-30 14:33:39 +0100

    Added simple starting example for aurelia.

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,5 @@
-import {RouterConfiguration, Router} from 'aurelia-router';
-
 export class App {
-  router: Router;
 
-  configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
-      { route: 'users',         name: 'users',    moduleId: './users',    nav: true, title:'Github Users' }
-    ]);
+  framework: string = 'Aurelia';
 
-    this.router = router;
-  }
 }

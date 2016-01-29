6c3e68fc51d940a2dee9baee20b3a845cc0b4200
Ben Millo <bmi@zuehlke.com>
2016-01-29 11:31:00 +0100

    Getting list from REST and display simple table

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
-export class App {
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
 
-    framework: string = 'Aurelia';
+@inject(HttpClient)
+export class App {    
+    hotels: any = undefined;
 
+    constructor(http) {
+    this.http = http;
+    this.retrieveHotels();   
+  };
+    
+    
+    retrieveHotels() {
+        this.http.fetch('http://localhost:1337/locations')
+            .then(response => response.json())
+            .then(data => {
+                this.hotels = data;
+            })
+            .catch(error => {
+            console.log('Error getting hotels from remote: ' + error.message);
+        });
+    };
 }
